270d2a8d2fe96de8df6f573952b1f69c71ab384c
Christoph Häni <christoph.haeni@bfh.ch>
2022-01-31 11:01:03 +0100

    Add source and sensor rotation methods

--- R/rotate.r
+++ R/rotate.r
@@ -1,11 +1,11 @@
 
 
 rotate <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x","y")){
-	ang <- -Angle/180
-	ctr <- Center
 	UseMethod("rotate",x)
 }
 rotate.default <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x","y")){
+	ang <- -Angle/180
+	ctr <- Center
 	if(!is.numeric(nms)){
 		nms <- match(nms,names(x),nomatch=0)
 		nms[!nms] <- (c(1,2) %w/o% nms)[seq_len(sum(nms==0))]		
@@ -18,6 +18,8 @@ rotate.default <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x",
 	return(out)
 }
 rotate.matrix <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x","y")){
+	ang <- -Angle/180
+	ctr <- Center
 	if(!is.numeric(nms)){
 		nms <- match(nms,colnames(x),nomatch=0)
 		nms[!nms] <- (c(1,2) %w/o% nms)[seq_len(sum(nms==0))]		
@@ -30,6 +32,8 @@ rotate.matrix <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x","
 	return(out)
 }
 rotate.data.table <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("x","y")){
+	ang <- -Angle/180
+	ctr <- Center
 	if(is.numeric(nms)){
 		nms <- names(x)[nms]
 	}
@@ -41,4 +45,18 @@ rotate.data.table <- function(x,xAxisTo=0,Center=c(0,0),Angle=xAxisTo-90,nms=c("
 	x[,nms[2] := xnms[,nms1] * sinpi(ang) + xnms[,nms2] * cospi(ang)  + ctr[2],with=FALSE]
 	invisible(x)
 }
-
+rotate.Sensors <- function(x, xAxisTo = 0, Center = c(0, 0), Angle = xAxisTo - 90, nms = c('x-Coord (m)', 'y-Coord (m)')) {
+	ang <- -Angle/180
+	ctr <- Center
+	if(!is.numeric(nms)){
+		nms <- match(nms,colnames(x),nomatch=0)
+		nms[!nms] <- (c(1,2) %w/o% nms)[seq_len(sum(nms==0))]		
+	}
+	x[,nms[1]] <- x[,nms[1]] - ctr[1]
+	x[,nms[2]] <- x[,nms[2]] - ctr[2]
+	out <- x
+	out[,nms[1]] <- x[,nms[1]] * cospi(ang) - x[,nms[2]] * sinpi(ang)  + ctr[1]
+	out[,nms[2]] <- x[,nms[1]] * sinpi(ang) + x[,nms[2]] * cospi(ang)  + ctr[2]
+	return(out)
+}
+rotate.Sources <- rotate.Sensors

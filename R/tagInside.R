86c0955479302e9bda785d192148c149d9033899
Christoph Häni <christoph.haeni@bfh.ch>
2023-04-14 14:07:06 +0200

    Always provide both logical inside and source names columns

--- R/tagInside.R
+++ R/tagInside.R
@@ -4,19 +4,34 @@
 #   - rename function 
 #       -> rename all existing tagInside() calls
 #   - better arguments/argument names
-#   - use better names for Sensors class (simpleNames)
+#   - use better names for Sensors class (switchNames)
 #       -> this must be switched right at beginning (in run_bls)
+#       -> same for Sources -> name, x, y, id
 #   - use tag_inside in calcCE.R ?
 #   - use better classes than 'Sensors', 'Sources', 'TDcat' ('bls_sensors', etc.?)
-tagInside <- function(catalog, tag_source, origin = c(0, 0),
-    tagBySourceName = tagPoly, tagPoly = FALSE) {
+
+#' tag touchdowns inside source areas
+#'
+#' tag touchdowns inside source areas. Adds columns
+#'  "tag_inside" and "source_names" to the TDcat object
+#'
+#' @param catalog A TDcat object, i.e., a bLSmodelR TD catalog of class TDcat.
+#' @param sources An object of class "Sources".
+#' @param origin Coordinates of the touchdown catalogs origin (sensor position) in 
+#'  the coordinates of the "Sources" object.
+#' @param tag_id Should polygon IDs be part of the source name? (Default: FALSE)
+#' @return The provided TDcat object with two columns named "tag_inside", indicating if
+#'  touchdowns are inside the sources or not (TRUE/FALSE), and "source_names", indicating
+#'  the name of the corresponding source where the touchdown hits inside.
+tagInside <- function(catalog, sources, origin = c(0, 0),
+    tag_id = FALSE) {
 
     if (!inherits(catalog, 'TDcat')) {
         stop('Argument "catalog" must be of class "TDcat"')
     }
 
-    if (!inherits(tag_source, 'Sources')) {
-        stop('Argument "tag_source" must be of class "Sources"')
+    if (!inherits(sources, 'Sources')) {
+        stop('Argument "sources" must be of class "Sources"')
     }
     
     if (inherits(origin, 'Sensors')) {
@@ -28,51 +43,47 @@ tagInside <- function(catalog, tag_source, origin = c(0, 0),
             ' point sensor (class "Sensors")')
     }
 
-	catalog[, tagInside := 
-        if (tagBySourceName) {
-            rep("", nrow(catalog))
-        } else {
-            rep(FALSE, nrow(catalog))
-        }
-    ]
-	source_relative <- data.table(tag_source)
-	setnames(source_relative, c("area", "x", "y", "pid"))
-	source_relative[, ":="(
+	catalog[, ':='(
+        tag_inside = FALSE,
+        source_names = ''
+        )]
+	sources_relative <- data.table(sources)
+	setnames(sources_relative, c("area", "x", "y", "pid"))
+	sources_relative[, ":="(
         x = x - origin[1],
         y = y - origin[2]
         )]
-	tag_bbox(catalog, source_relative)
-	catalog[, inside0 := inside]
+	tag_bbox(catalog, sources_relative)
 	catalog[, rn := .I]
 	setkey(catalog, rn)
 
 	if (catalog[, any(inside)]) {
 		# tag Inside Source
-		if (tagBySourceName) {
-			if (tagPoly) {
-				TDinside <- source_relative[,
-				{
-					tag_bbox(catalog[, inside := inside0], .(x = x, y = y))
-					cbind(ID = catalog[(inside), rn], pnt.in.poly(catalog[(inside), cbind(x, y)], cbind(x, y)))
-				}, by = .(area, pid)][pip == 1L, paste0(paste(area, pid, sep = ","), collapse = ";"), by = ID]	
-			} else {
-				TDinside <- source_relative[,
-				{
-					tag_bbox(catalog[, inside := inside0], .(x = x, y = y))
-					cbind(ID = catalog[(inside), rn], pnt.in.poly(catalog[(inside), cbind(x, y)], cbind(x, y)))
-				}, by = .(area, pid)][pip == 1L, paste0(area, collapse = ","), by = ID]					
-			}
-			catalog[TDinside, tagInside := V1]
-		} else {
-			TDinside <- source_relative[,
-			{
-				tag_bbox(catalog[, inside := inside0], .(x = x, y = y))
-				cbind(ID = catalog[(inside), rn], pnt.in.poly(catalog[(inside), cbind(x, y)], cbind(x, y)))
-			}, by = .(area, pid)][, sum(pip), by = ID]
-			catalog[TDinside, tagInside := as.logical(V1)]
-		}
+        if (tag_id) {
+            td_inside <- sources_relative[, {
+                cbind(
+                    ID = catalog[(inside), rn], 
+                    pnt.in.poly(catalog[(inside), cbind(x, y)], cbind(x, y))
+                )
+            }, by = .(area, pid)][pip == 1L, 
+            paste0(paste(area, pid, sep = "/"), collapse = ",")
+            , by = ID]	
+        } else {
+            td_inside <- sources_relative[, {
+                cbind(
+                    ID = catalog[(inside), rn], 
+                    pnt.in.poly(catalog[(inside), cbind(x, y)], cbind(x, y))
+                )
+            }, by = .(area, pid)][pip == 1L, 
+            paste0(area, collapse = ",")
+            , by = ID]					
+        }
+        catalog[td_inside, ':='(
+            tag_inside = TRUE,
+            source_names = V1
+            )]
 	}
-	catalog[, ":="(inside = NULL, inside0 = NULL, rn = NULL)]
+	catalog[, ":="(inside = NULL, rn = NULL)]
 
 	invisible(catalog)
 }

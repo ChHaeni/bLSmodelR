a283ac8a3a7c768a50d674e70bf441e9ef324280
Christoph Häni <christoph.haeni@bfh.ch>
2023-07-28 12:33:00 +0200

    Change write/read Catalog to new binary format

--- R/readCatalog.R
+++ R/readCatalog.R
@@ -1,6 +1,32 @@
-readCatalog <- function(Name, as.is = FALSE) {
-	# get data:
-	Ctlg <- alloc.col(qs::qread(Name))
+readCatalog <- function(Name, as.is = FALSE, header_only = FALSE) {
+    # read from binary file
+    on.exit(close(con))
+    con <- file(Name, open = 'rb')
+
+    # check flag
+    sig <- readBin(con, 'character', endian = 'little')
+    if (sig != 'A') {
+        return(NULL)
+    }
+    # get length of header
+    r_h_length <- readBin(con, 'integer', size = 4, endian = 'little')
+    # get length of catalog
+    r_c_length <- readBin(con, 'numeric', size = 8, endian = 'little')
+    # read header
+    r_h <- readBin(con, 'raw', n = r_h_length, endian = 'little')
+    if (header_only) {
+        # unserialize header
+        header <- qs::qdeserialize(r_h, strict = TRUE)
+        class(header) <- c('TDhead', 'character')
+        return(header)
+    }
+
+    # read catalog
+    r_c <- readBin(con, 'raw', n = r_c_length, endian = 'little')
+    # unserialize catalog
+    Ctlg <- alloc.col(qs::qdeserialize(r_c, strict = TRUE))
+
+    # decompact sizes
 	if (!as.is & attr(Ctlg, "is.int")) compactCatalog(Ctlg, as.int = FALSE)
-	return(Ctlg)
+    Ctlg
 }
--- R/writeCatalog.R
+++ R/writeCatalog.R
@@ -1,5 +1,28 @@
 writeCatalog <- function(Ctlg, Name, compactTDcat = TRUE, ...) {
-	# write data:
+	# reduce sizes
 	if (compactTDcat) compactCatalog(Ctlg, ...)
-	qs::qsave(Ctlg, Name, 'balanced')
+
+    # serialize first
+    ## header (raw or serialized?)
+    h_r <- attr(Ctlg, 'header')[[1]]
+    h_s <- qs::qserialize(h_r, 'balanced')
+    h_s_length <- length(h_s)
+    ## entire catalog
+    c_s <- qs::qserialize(Ctlg, 'balanced')
+    c_s_length <- length(c_s)
+
+    # write binary
+    # open file connection
+    on.exit(close(con))
+    con <- file(Name, open = 'wb')
+    # flag for new format
+    writeBin('A', con, endian = 'little')
+    # length of header
+    writeBin(as.integer(h_s_length), size = 4, con, endian = 'little')
+    # length of catalog
+    writeBin(as.numeric(c_s_length), size = 8, con, endian = 'little')
+    # header
+    writeBin(h_s, con, endian = 'little')
+    # catalog
+    writeBin(c_s, con, endian = 'little')
 }

72d941dd8a49e93c53aad00bc21e62ff183f590a
Christoph Häni <christoph.haeni@bfh.ch>
2025-05-16 17:32:33 +0200

    Import RgoogleMaps and geoshpere functions only if needed

--- R/addScaleBar.R
+++ R/addScaleBar.R
@@ -8,6 +8,9 @@ addScaleBar <- function(pos=3,scale=1,units="meters",frac=5,cuts=c(1,2,5),minors
 		if(!requireNamespace("geosphere", quietly = TRUE)){
 			stop("please install package geosphere: install.packages('geosphere')")
 		}
+		if(!requireNamespace("RgoogleMaps", quietly = TRUE)){
+			stop("please install package RgoogleMaps: install.packages('RgoogleMaps')")
+		}
 		USR <- XY2LatLon(scale,usr[1:2],usr[3:4])
 		scale <- sqrt((usr[1]-usr[2])^2+(usr[3]-usr[4])^2)/distGeo(USR[1,2:1],USR[2,2:1])
 	}

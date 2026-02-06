ChHaeni/bLSmodelR

commit 127fbbc5bd3de37d75449acc542ba290ee7402af

Try to catch old qs format of catalog file

R/rebuildCatListFile.r
@@ -4,9 +4,9 @@ rebuildCatListFile <- function(C.Path, fromScratch = FALSE, ncores = NULL) {
     # check if cats exist & list them
 	Existing <- dir(C.Path, pattern = "Cat_Zm.*_[0-9]{14}$")
     # define path to CatList file
-	Catfile <- file.path(C.Path, ".catalogs")
+	Catfile <- file.path(C.Path, ".blsmodelr")
     # check if exists otherwise try old names
-    legacy_names <- '.cats'
+    legacy_names <- c('.catalogs', '.cats')
     if (Catfile_exists <- file.exists(Catfile)) {
         # remove old file names
         for (nm in legacy_names) {
@@ -18,7 +18,16 @@ rebuildCatListFile <- function(C.Path, fromScratch = FALSE, ncores = NULL) {
         # try old file names & rename (latest version has highest priority)
         for (nm in legacy_names) {
             if (file.exists(file.path(C.Path, nm))) {
-                Catfile_exists <- suppressWarnings(file.rename(file.path(C.Path, nm), Catfile))
+                # fix old qs dependence
+                if (Catfile_exists <- requireNamespace('qs')) {
+                    # read
+                    cf <- qs::qread(file.path(C.path, nm))
+                    # save
+                    qs2::qd_save(Catfile)
+                } else {
+                    # remove old file
+                    file.remove(file.path(C.Path, nm))
+                }
             }
         }
     }

commit aeeee24989d99a150f1d5e19a2e614fd6bcbcd7a

Fix errors introduced with 127fbbc

R/rebuildCatListFile.r
@@ -21,9 +21,9 @@ rebuildCatListFile <- function(C.Path, fromScratch = FALSE, ncores = NULL) {
                 # fix old qs dependence
                 if (Catfile_exists <- requireNamespace('qs')) {
                     # read
-                    cf <- qs::qread(file.path(C.path, nm))
+                    cf <- qs::qread(file.path(C.Path, nm))
                     # save
-                    qs2::qd_save(Catfile)
+                    qs2::qd_save(cf, Catfile)
                 } else {
                     # remove old file
                     file.remove(file.path(C.Path, nm))